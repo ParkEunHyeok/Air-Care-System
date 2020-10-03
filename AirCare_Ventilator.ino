9c705c39c597c20990760eea33ecbbc4ab95165c
ParkEunHyeok <35924139+ParkEunHyeok@users.noreply.github.com>
2020-10-04 03:46:12 +0900

    Add files via upload

---
+++ AirCare_Ventilator.ino
@@ -0,0 +1,77 @@
+#include <esp_now.h>
+#include <WiFi.h>
+
+// Replace with your network credentials (STATION)
+const char* ssid = "olleh_WiFi_215A";
+const char* password = "000000486a";
+
+typedef struct esp_struct {
+  int x;
+  int y;
+} esp_struct;
+//Create a struct_message called myData
+esp_struct esp_received;
+
+// callback function that will be executed when data is received
+void OnDataRecv(const uint8_t * mac_addr, const uint8_t *incomingData, int len) { 
+  // Copies the sender mac address to a string
+  char macStr[18];
+  Serial.print("Packet received from: ");
+  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+  Serial.println(macStr);
+  
+  memcpy(&esp_received, incomingData, sizeof(esp_received));
+  Serial.print("Bytes received: ");
+  Serial.println(len);
+  Serial.print("x: ");
+  Serial.println(esp_received.x);
+  Serial.print("y: ");
+  Serial.println(esp_received.y);
+  Serial.println();
+
+  if(esp_received.y == 1) {
+    Serial.print("HIGH");
+    digitalWrite(17, HIGH);
+  } else {
+    digitalWrite(17, LOW);
+  }
+}
+
+void setup() {
+  // Initialize Serial Monitor
+  Serial.begin(115200);
+
+  // Set the device as a Station and Soft Access Point simultaneously
+  WiFi.mode(WIFI_AP_STA);
+  
+  // Set device as a Wi-Fi Station
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Setting as a Wi-Fi Station..");
+  }
+  Serial.print("Station IP Address: ");
+  Serial.println(WiFi.localIP());
+  Serial.print("Wi-Fi Channel: ");
+  Serial.println(WiFi.channel());
+
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+  
+  // Once ESPNow is successfully Init, we will register for recv CB to
+  // get recv packer info
+  esp_now_register_recv_cb(OnDataRecv);
+  pinMode(17,OUTPUT);
+}
+ 
+void loop() {
+  static unsigned long lastEventTime = millis();
+  static const unsigned long EVENT_INTERVAL_MS = 5000;
+  if ((millis() - lastEventTime) > EVENT_INTERVAL_MS) {
+    lastEventTime = millis();
+  }
+}
